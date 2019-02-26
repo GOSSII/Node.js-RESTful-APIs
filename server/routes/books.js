70c88238afcab683ccb81a4c610e452937b47cec
Dipesh Goswami <gossi@Dipeshs-MacBook-Pro.local>
2019-02-26 11:32:45 -0500

    setup Environment

---
+++ server/routes/books.js
@@ -0,0 +1,32 @@
+// modules required for routing
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+// GET the Book Details page in order to edit an existing Book
+router.get('/:id', (req, res, next) => {
+
+    /*****************
+     * ADD CODE HERE *
+     *****************/
+});
+
+// POST - process the information passed from the details form and update the document
+router.post('/:id', (req, res, next) => {
+
+    /*****************
+     * ADD CODE HERE *
+     *****************/
+
+});
+
+// GET - process the delete by user id
+router.get('/delete/:id', (req, res, next) => {
+
+    /*****************
+     * ADD CODE HERE *
+     *****************/
+});
+
+
+module.exports = router;
