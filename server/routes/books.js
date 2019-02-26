57510611b82f31fab9ed44906d9ae1c25cb0cd63
Dipesh Goswami <gossi@Dipeshs-MacBook-Pro.local>
2019-02-26 11:36:12 -0500

    GET - List of Books

--- server/routes/books.js
+++ server/routes/books.js
@@ -3,6 +3,41 @@ let express = require('express');
 let router = express.Router();
 let mongoose = require('mongoose');
 
+// define the book model
+let bookModel = require('../models/books');
+
+/* GET books List page. READ */
+router.get('/', (req, res, next) => {
+  // find all books in the books collection
+  bookModel.find( (err, books) => {
+    if (err) {
+      return console.error(err);
+    }
+    else {
+      // res.render('books/index', {
+      //   title: 'Books',
+      //   books: books
+      // });
+    res.json(books);
+    }
+  });
+
+});
+
+//  GET the Book Details page in order to add a new Book
+router.get('/add', (req, res, next) => {
+  res.render('books/details', {
+    title : "Add A Book Here",
+    books : bookModel
+  })
+})
+
+
+// POST process the Book Details page and create a new Book - CREATE
+router.post('/add', (req, res, next) => {
+  
+});
+
 // GET the Book Details page in order to edit an existing Book
 router.get('/:id', (req, res, next) => {
 
